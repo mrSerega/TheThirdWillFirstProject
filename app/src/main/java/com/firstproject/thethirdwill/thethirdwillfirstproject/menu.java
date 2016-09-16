96341be31336b58d846f4b30d9a945b5747fd99b
mrSerega <sega00796@mail.ru>
2016-09-16 19:41:28 +0300

    question
    +question

--- app/src/main/java/com/firstproject/thethirdwill/thethirdwillfirstproject/menu.java
+++ app/src/main/java/com/firstproject/thethirdwill/thethirdwillfirstproject/menu.java
@@ -1,13 +1,25 @@
 package com.firstproject.thethirdwill.thethirdwillfirstproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class menu extends AppCompatActivity {
+    //declare widgets
+    Button btn_play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        //init widgets
+        btn_play = (Button) findViewById(R.id.btn_play);
+    }
+
+    public void btn_play_func()
+    {
+        Intent newIntent = new Intent(menu.this, question.class);
+        startActivity(newIntent);
     }
 }
